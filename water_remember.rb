0c6fdef0e10a213f27132e59b692db0ebfc0da89
Djalma araujo <djalma.araujo@gmail.com>
2013-07-04 17:50:31 -0300

    drink water

---
+++ water_remember.rb
@@ -0,0 +1,24 @@
+require 'terminal-notifier'
+
+class WaterRemember
+  attr_reader :message
+
+  def initialize()
+    @message = "It's time to drink water"
+
+    notify
+  end
+
+  private
+
+  def notify
+    TerminalNotifier.notify(message, execute: say_drink_water)
+    say_drink_water
+  end
+
+  def say_drink_water
+    'say "Drink water dude!"'
+  end
+end
+
+WaterRemember.new()
